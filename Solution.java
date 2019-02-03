40758ab3c1cb171d60cb64163b4a0ac0107cd063
sdim-ua <ingersol@yandex.ua>
2019-02-03 22:15:06 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,23 @@
+
+/* 
+Три статические переменных name
+
+
+*/
+
+public class Solution {
+
+    public static String name;
+
+    public static class Cat {
+        public static String name;
+    }
+
+    public static class Dog {
+        public static String name;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
